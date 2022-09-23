62e805fea6ae0a13cb98306bc8366f53da8bd7ce
gustavoponciano <96.gustavop@gmail.com>
2022-09-23 00:00:42 -0300

    final commit

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,8 +17,8 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
-            string placaCliente = Console.ReadLine();//Gus: adicionado
-            veiculos.Add(placaCliente); //Gus: adicionado
+            string placaCliente = Console.ReadLine();
+            veiculos.Add(placaCliente);
         }
 
         public void RemoverVeiculo()
@@ -27,29 +27,26 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = Console.ReadLine(); //Gus: adicionado
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
                 int horas = 0;
-                decimal valorTotal = 0;
+                // decimal valorTotal = 0;
 
-                /* 
-                
-                a. preciso receber o input Console.ReadLine() e add na variável horas (acho)
-                b. fazer o cálculo
-                                
-                */
+                horas = Convert.ToInt32(Console.ReadLine());
+
+                decimal valorTotal = precoInicial + precoPorHora * horas;
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-                veiculos.Remove(Console.ReadLine()); //Gus: adicionei
+                veiculos.Remove(placa);
 
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
